58da0a569cc276fea44c725a8da19e7b28e50ce8
Sp3c1 <spectator.networx@gmail.com>
2015-12-10 16:07:47 +0100

    chore(deploy): add deploy script.

---
+++ tools/deploy.js
@@ -0,0 +1,12 @@
+/**
+ * This configuration will suppress logging and sanitize error messages.
+ */
+var ghpages = require('gh-pages');
+var path = require('path');
+ghpages.publish(path.join(__dirname, 'build'), {
+  repo: 'https://' + process.env.GH_TOKEN +
+    '@github.com/jsunconf/2016.jsunconf.eu.git',
+  silent: true
+}, function(error){
+  console.log(error);
+});
